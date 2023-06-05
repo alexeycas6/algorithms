83ef04cbec454f1f514870c74bfa1eb79e5ada4f
Alexey Sinitskiy <sinitskiyai@mail.ru>
2023-06-05 14:23:57 +0500

    added binary search

---
+++ src/binary_search.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class solution {
+private:
+    bool good(int val, int target) { return val <= target; }
+    /*
+     * array: 1 2 4 7 9 15
+     * target: 7
+     * good:  t t t t f f
+     */
+public:
+    int binary_search(const vector<int>& nums, int target) {
+        int left = -1;
+        int right = nums.size();
+
+        while (right - left > 1) {
+            int middle = left + (right - left) / 2;
+            if (good(nums[middle], target))
+                left = middle;
+            else
+                right = middle;
+        }
+
+        if (nums[left] == target)
+            return left;
+        else
+            return -1;
+    }
+};
+
+int main() {
+    std::vector<int> nums = { 1, 2, 4, 7, 9, 15 };
+    int target = 7;
+
+    solution s;
+    auto idx = s.binary_search(nums, 7);
+    std::cout << idx << " ";
+
+    return 0;
+}
